dac5678145cf34cea2315bd0056f741dc76ca3a4
sebitservices <admin@sebitservices.cl>
2024-12-01 01:57:34 -0300

    feat: Actualizacion server.js

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        'https://admintechflow.com/',
+        'http://localhost:5500' // para desarrollo local
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ extended: true, limit: '500mb' }));
 
